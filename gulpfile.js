a096cf2f04230929f7a785666f2cea6b48b92454
Alexander Skovpen <a.n.skovpen@gmail.com>
2014-05-28 14:23:22 +0400

    rebase project

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@ var replace = require('gulp-replace');
 var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
 var exec = require('child_process').exec;
+var mocha = require('gulp-mocha');
 var js=['./package.json',
 	'./gulpfile.js',
 	'./fido-squish.js',
 	'./test/test.js'
-	];
+];
 gulp.task('version', function(done){
 	exec('/usr/bin/git rev-list HEAD --count', function(err, stdout, stderr){
 		vers='0.0.'+stdout.replace(/(\r\n|\n|\r)/gm,"");
@@ -19,11 +20,19 @@ gulp.task('replace',['version'], function() {
 	gulp.src('package.json.tpl')
 		.pipe(replace('@@ver', vers))
 		.pipe(rename('package.json'))
-		.pipe(gulp.dest('./'));
+		.pipe(gulp.dest('./dist'));
 });
 gulp.task('lint',['replace'], function() {
 	return gulp.src(js)
 		.pipe(jshint.reporter('jshint-stylish'))
-		.pipe(jshint.reporter('fail'))
+		.pipe(jshint.reporter('fail'));
 });
-gulp.task('default', ['lint']);
+gulp.task('test',['lint'], function() {
+    gulp.src('test/test.js')
+	.pipe(mocha({timeout:'60s',reporter:'spec'}));
+});
+gulp.task('copy',['test'], function() {
+	gulp.src(['README.md','fidonet-squish.js','LICENSE'])
+		.pipe(gulp.dest('./dist'));
+});
+gulp.task('default', ['copy']);
--- package.json.tpl
+++ package.json.tpl
@@ -7,21 +7,10 @@
 	"author": { "name": "Alexander N. Skovpen" },
 	"dependencies": {
 		"moment": "~2.3.0",
-		"singlebyte": "~1.0.10"
-	},
-	"devDependencies": {
-		"gulp": ">0.0.0",
-		"gulp-replace": ">0.0.0",
-		"gulp-jshint": ">0.0.0",
-		"gulp-rename": ">0.0.0",
-		"jshint-stylish": ">0.0.0"
+		"singlebyte": "~1.0.11"
 	},
 	"repository": {
 		"type": "git",
 		"url": "https://github.com/askovpen/node-fidonet-squish.git"
-	},
-	"scripts": {
-		"pretest": "jshint fidonet-squish.js test/test.js",
-		"test": "mocha --reporter spec --timeout 60s"
 	}
 }
